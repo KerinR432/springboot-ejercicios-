89e90f202d2ae596a00a162e7d5fca0cf32b5bfd
KerinR432 <kerinaguilera@gmail.com>
2025-10-14 10:43:01 +0200

    Limpieza de archivos ignorados

---
+++ SinValidar/src/main/java/Validar/SinValidar/SinValidarApplication.java
@@ -0,0 +1,13 @@
+package Validar.SinValidar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinValidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinValidarApplication.class, args);
+	}
+
+}
---
+++ SinValidar/src/test/java/Validar/SinValidar/SinValidarApplicationTests.java
@@ -0,0 +1,13 @@
+package Validar.SinValidar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinValidarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
