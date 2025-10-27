7a2b327a4b9097f330b0dfc7261eee13265143ee
KerinR432 <kerinaguilera@gmail.com>
2025-10-27 10:19:46 +0100

    cookees

---
+++ Cookies/src/test/java/com/R3_UD2/ProyectoR3UD2/ProyectoR3Ud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.R3_UD2.ProyectoR3UD2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoR3Ud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
