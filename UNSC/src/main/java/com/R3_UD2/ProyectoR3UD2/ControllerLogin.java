3f46135123738a8c7d5ae59d4a6161716ec6adc0
Ruvik <Kerinaguilera@gmail.com>
2025-11-07 19:49:08 +0100

    credenciales

--- UNSC/src/main/java/com/R3_UD2/ProyectoR3UD2/ControllerLogin.java
+++ UNSC/src/main/java/com/R3_UD2/ProyectoR3UD2/ControllerLogin.java
@@ -2,33 +2,73 @@ package com.R3_UD2.ProyectoR3UD2;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerLogin {
 
 
+    //* creamos variables globales
     static String usuario = "Fernando";
     static String pass = "1234";
+
+
+    //*Muestra la pagina principal, comprueba si existe las credencias
     @GetMapping("/")
-    public String paginaPrincipal(HttpSession sesion){
-        if(sesion.getAttribute("nombre")!= usuario && sesion.getAttribute("contraseña")!=pass){
-             return "login";
+    public String paginaPrincipal(HttpSession sesion) {
+        if (!usuario.equals(sesion.getAttribute("nombre")) || !pass.equals(sesion.getAttribute("contraseña"))) {
+            return "redirect:/login";
         }
-        System.out.println("El nombre es:"+sesion.getAttribute("nombre"));
-        return "pagina1";
+        System.out.println("El nombre es:" + sesion.getAttribute("nombre"));
+        return "redirect:/credenciales";
     }
 
-    @PostMapping("/login")
-    public String paginaCrendecial(HttpSession sesion, @RequestParam(required = false) String nombre,
-                                   @RequestParam(required = false) String contraseña) {
+    @GetMapping("/login")
+    public String paginaLogiarse(HttpSession sesion, @RequestParam(required = false) String nombre,
+                                 @RequestParam(required = false) String contraseña) {
+        if (usuario.equals(nombre) && pass.equals(contraseña)) {
+            sesion.setAttribute("nombre", nombre);
+            sesion.setAttribute("contraseña", contraseña);
+            return "redirect:/credenciales";
+        }
+        return "login";
+    }
 
-        sesion.setAttribute("nombre",nombre);
-        sesion.setAttribute("contraseña",contraseña);
-        System.out.printf((String) sesion.getAttribute("nombre"));
-        System.out.printf((String) sesion.getAttribute("contraseña"));
-        return (String)sesion.getAttribute("nombre");
+    @GetMapping("/credenciales")
+    public String credenciales(HttpSession sesion, Model modelo,
+                               @RequestParam(value = "bton", required = false) String valorBoton,
+                               @RequestParam(value = "bton2", required = false) String valorBoton2) {
+        if (!usuario.equals(sesion.getAttribute("nombre")) || !pass.equals(sesion.getAttribute("contraseña"))) {
+            return "redirect:/login";
+        }
+        modelo.addAttribute("nombreUsuario", sesion.getAttribute("nombre"));
+        if ("misiones".equals(valorBoton)) {
+            System.out.println("estoy dentro");
+            return "redirect:/pagina2";
+        }
+        if("Cerrar Sesión".equals(valorBoton2)){
+            return "redirect:/cerrarSesion";
+        }
+        return "credenciales";
+    }
+
+    @GetMapping("/pagina2")
+    public String misiones(HttpSession sesion, @RequestParam(value = "bton1", required = false) String valorBton1,
+                           @RequestParam(value = "bton2",required = false) String valorBton2) {
+        if("Credenciales".equals(valorBton1)){
+            return "redirect:/credenciales";
+        }
+        if("Cerrar Sesión".equals(valorBton2)){
+            return "redirect:/cerrarSesion";
+        }
+        return "pagina2";
+    }
+
+    @GetMapping("/cerrarSesion")
+    public String cerrarSesion(HttpSession sesion){
+        sesion.invalidate();
+        return "redirect:/login";
     }
 }
