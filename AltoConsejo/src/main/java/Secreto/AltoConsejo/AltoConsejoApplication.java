47793ac81bdcd1d6def989cd5631ef4005b7cb57
KerinR432 <kerinaguilera@gmail.com>
2025-11-21 14:14:57 +0100

    subir datos

---
+++ AltoConsejo/src/main/java/Secreto/AltoConsejo/AltoConsejoApplication.java
@@ -0,0 +1,13 @@
+package Secreto.AltoConsejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltoConsejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltoConsejoApplication.class, args);
+	}
+
+}
---
+++ AltoConsejo/src/test/java/Secreto/AltoConsejo/AltoConsejoApplicationTests.java
@@ -0,0 +1,13 @@
+package Secreto.AltoConsejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltoConsejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
